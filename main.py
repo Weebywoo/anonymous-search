24dce938c7beda6c11db19bfd25ea1d1a442e909
Weebywoo <kosmokunert@gmail.com>
2024-11-16 21:53:12 +0100

    vercel deployment fix

--- main.py
+++ main.py
@@ -5,6 +5,3 @@ from src.api import router as api_router
 
 app: FastAPI = FastAPI()
 app.include_router(api_router)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
